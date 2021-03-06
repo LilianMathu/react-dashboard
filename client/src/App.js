4d447678c4e07d34124802356fbc894b30195aae
lilian Mathu <lilianmathu2018@gmail.com>
2021-03-06 13:30:20 +0300

    add avatar and heading to header

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Layout, Typography, Avatar, Image } from 'antd';
 import './App.css';
 
 const { Header, Footer, Sider, Content } = Layout;
+const { Title } = Typography;
+
+
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <Sider style={{backgroundColor:'blueviolet'}}>Sider</Sider>
+        <Header style={{padding:10, backgroundColor:"#ffebe6"}}>
+        <Avatar style={{float: "right"}} src="https://res.cloudinary.com/dcltsh8p7/image/upload/v1607930587/lily_tb9omz.jpg" />
+        <Title level={3}>Mobile Mechanics</Title>
+        </Header>
         <Layout>
-          <Header>Header</Header>
-          <Content>Content</Content>
-          <Footer>Footer</Footer>
+          <Sider style={{ backgroundColor: 'blueviolet' }}>Sider</Sider>
+          <Layout>
+            <Content>Content</Content>
+            <Footer>Footer</Footer>
+          </Layout>
         </Layout>
       </Layout>
-    </div>
+    </div> 
   );
 }
 
