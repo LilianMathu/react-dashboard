068b2d115c333753fa1fe9091fd2b0126bbc7bf5
lilian Mathu <lilianmathu2018@gmail.com>
2021-03-07 11:05:06 +0300

    collapse error

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,145 @@
-import React from 'react';
-import { Layout, Typography, Avatar, Image } from 'antd';
-import './App.css';
+import React, { useState } from 'react';
+import { Layout, Typography, Avatar, Menu, Button } from 'antd';
+import {
+  AppstoreOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  ContainerOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
 
+const { SubMenu } = Menu;
 const { Header, Footer, Sider, Content } = Layout;
 const { Title } = Typography;
 
 
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed)
+  };
+
   return (
     <div className="App">
       <Layout>
-        <Header style={{padding:10, backgroundColor:"#ffebe6"}}>
-        <Avatar style={{float: "right"}} src="https://res.cloudinary.com/dcltsh8p7/image/upload/v1607930587/lily_tb9omz.jpg" />
-        <Title level={3}>Mobile Mechanics</Title>
+        <Header style={{ padding: 10, backgroundColor: "#ffebe6" }}>
+          <Avatar style={{ float: "right" }} src="https://res.cloudinary.com/dcltsh8p7/image/upload/v1607930587/lily_tb9omz.jpg" />
+          <Title level={3}>Mobile Mechanics</Title>
         </Header>
+
         <Layout>
-          <Sider style={{ backgroundColor: 'blueviolet' }}>Sider</Sider>
+          <Sider>
+          <div style={{ width: 256 }}>
+        <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
+          {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
+        </Button>
+        <Menu
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          theme="dark"
+          inlineCollapsed={collapsed}
+        >
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <Menu.Item key="3" icon={<ContainerOutlined />}>
+            Option 3
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+            <Menu.Item key="5">Option 5</Menu.Item>
+            <Menu.Item key="6">Option 6</Menu.Item>
+            <Menu.Item key="7">Option 7</Menu.Item>
+            <Menu.Item key="8">Option 8</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
+            <Menu.Item key="9">Option 9</Menu.Item>
+            <Menu.Item key="10">Option 10</Menu.Item>
+            <SubMenu key="sub3" title="Submenu">
+              <Menu.Item key="11">Option 11</Menu.Item>
+              <Menu.Item key="12">Option 12</Menu.Item>
+            </SubMenu>
+          </SubMenu>
+        </Menu>
+      </div>
+          </Sider>
           <Layout>
             <Content>Content</Content>
             <Footer>Footer</Footer>
           </Layout>
         </Layout>
       </Layout>
-    </div> 
+    </div>
   );
 }
 
 export default App;
+
+
+// import React, {useState} from 'react';
+// import { Menu, Button } from 'antd';
+// import {
+//   AppstoreOutlined,
+//   MenuUnfoldOutlined,
+//   MenuFoldOutlined,
+//   PieChartOutlined,
+//   DesktopOutlined,
+//   ContainerOutlined,
+//   MailOutlined,
+// } from '@ant-design/icons';
+
+// const { SubMenu } = Menu;
+
+// export default function App() {
+//   const [collapsed, setCollapsed] = useState(false)
+
+//   const toggleCollapsed = () => {
+//     setCollapsed(!collapsed)
+//   };
+
+//     return (
+//       <div style={{ width: 256 }}>
+//         <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
+//           {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
+//         </Button>
+//         <Menu
+//           defaultSelectedKeys={['1']}
+//           defaultOpenKeys={['sub1']}
+//           mode="inline"
+//           theme="dark"
+//           inlineCollapsed={collapsed}
+//         >
+//           <Menu.Item key="1" icon={<PieChartOutlined />}>
+//             Option 1
+//           </Menu.Item>
+//           <Menu.Item key="2" icon={<DesktopOutlined />}>
+//             Option 2
+//           </Menu.Item>
+//           <Menu.Item key="3" icon={<ContainerOutlined />}>
+//             Option 3
+//           </Menu.Item>
+//           <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+//             <Menu.Item key="5">Option 5</Menu.Item>
+//             <Menu.Item key="6">Option 6</Menu.Item>
+//             <Menu.Item key="7">Option 7</Menu.Item>
+//             <Menu.Item key="8">Option 8</Menu.Item>
+//           </SubMenu>
+//           <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
+//             <Menu.Item key="9">Option 9</Menu.Item>
+//             <Menu.Item key="10">Option 10</Menu.Item>
+//             <SubMenu key="sub3" title="Submenu">
+//               <Menu.Item key="11">Option 11</Menu.Item>
+//               <Menu.Item key="12">Option 12</Menu.Item>
+//             </SubMenu>
+//           </SubMenu>
+//         </Menu>
+//       </div>
+//     );
+// }
